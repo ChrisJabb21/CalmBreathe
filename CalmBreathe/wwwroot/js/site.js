774c381e700a6a968a2478b255110e81e58ea536
ChrisJabb21 <chrisjabbour94@gmail.com>
2020-01-09 20:31:41 -0500

    Add project files.

---
+++ CalmBreathe/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+/*JS for slide animation*/
+
+$(document).ready(function () {
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function () {
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function () {
+        $("#myCarousel").carousel(1);
+    });
+   
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function () {
+        $("#myCarousel").carousel("next");
+    });
+}
